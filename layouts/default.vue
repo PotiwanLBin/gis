271235008461b2cb07853adb7bf265ff05379b1a
PotiwanLBin <50204757+PotiwanLBin@users.noreply.github.com>
2021-04-30 23:58:38 +0700

    add

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,11 +10,7 @@
 			>
 			</div>
 
-<<<<<<< Updated upstream
-        <v-toolbar-title class="mx-auto">Visualizing Air Pollution (PM2.5)</v-toolbar-title>
-=======
-			<v-toolbar-title class="mx-auto">Spatial And GIS</v-toolbar-title>
->>>>>>> Stashed changes
+			<v-toolbar-title class="mx-auto">Visualizing Air Pollution (PM2.5)</v-toolbar-title>
 
 			<!-- <template v-slot:extension v-if="$route.name == 'report'">
 			<v-tabs v-model="tab" align-with-title>
